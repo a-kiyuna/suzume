1ecb2d4a12f14c0cdd37321217a260258cd3c0cd
Asahi Kiyuna <a-kiyuna@netwrk.co.jp>
2024-07-13 15:40:33 +0900

    セッションにプレイヤーデータを格納するようにした

--- src/jp/co/netwrk/a_kiyuna/RegisterController.java
+++ src/jp/co/netwrk/a_kiyuna/RegisterController.java
@@ -1,6 +1,8 @@
 package jp.co.netwrk.a_kiyuna;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +10,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.co.netwrk.a_kiyuna.entity.Player;
 
 @WebServlet("/register")
 public class RegisterController extends HttpServlet {
@@ -23,29 +28,37 @@ public class RegisterController extends HttpServlet {
 		// 文字コードを設定
 		req.setCharacterEncoding("utf-8");
 
-		// 登録されているプレイヤーの数
-		int count = 0;
+		// 登録されているプレイヤー
+		List<Player> players = new ArrayList<>();
 		for (int i = 1; i <= 4; i++) {
 			// 入力内容保持用に設定
 			String name = req.getParameter("player" + i);
 			req.setAttribute("player" + i, name);
 
-			// 名前が空白でないならカウントする
+			// 名前が空白でないなら登録する
 			if (name != null && !name.isEmpty()) {
-				count++;
+				Player player = new Player(name, 0);
+
+				players.add(player);
 			}
 		}
 
 		// ３人未満ならエラーを設定し、同じページに移動
 		// そもそも３人登録しないとボタンが押せないのでこのエラーが起こることはないが、念の為実装。
-		if (count < 3) {
+		if (players.size() < 3) {
 			req.setAttribute("error", "プレイヤーを3人以上登録してください。");
 			RequestDispatcher rd = req.getRequestDispatcher("PlayerRegister.jsp");
 			rd.forward(req, res);
 			return;
 		}
 
-		// スタートできるならスコアボードページに移動
+		// スタートできる場合
+
+		// セッションにデータを保管
+		HttpSession session = req.getSession();
+		session.setAttribute("players", players);
+
+		// リダイレクト
 		res.sendRedirect("/suzume/scoreboard");
 	}
 
--- src/jp/co/netwrk/a_kiyuna/ScoreboardController.java
+++ src/jp/co/netwrk/a_kiyuna/ScoreboardController.java
@@ -8,12 +8,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/scoreboard")
 public class ScoreboardController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+
+		// セッションにプレイヤーデータがない場合、登録画面にリダイレクトさせる。
+		if (session.getAttribute("players") == null) {
+			res.sendRedirect("/suzume/register");
+			return;
+		}
+
 		RequestDispatcher rd = req.getRequestDispatcher("Scoreboard.jsp");
 		rd.forward(req, res);
 	}
